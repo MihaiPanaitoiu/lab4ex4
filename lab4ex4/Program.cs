37510b4c95498583e4d4f55ab450ef00acc14411
mihai.panaitoiu <mihai.panaitoiu@softem.ro>
2023-01-08 23:02:06 +0200

    Add project files.

---
+++ lab4ex4/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace lab4ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Ex 4
+            Scrieti o functie recursiva care va afisa in ordine elementele unui vector de
+            intregi.
+             */
+            Console.WriteLine("introduceti n");
+
+            int[] mat = ReadMatrix(int.Parse(Console.ReadLine()));
+
+            ShowMatNumbers(mat);
+        }
+
+        static int[] ReadMatrix(int n)
+        {
+            Console.WriteLine("introduceti elementele arrayului");
+            int[] result = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                result[i] = int.Parse(Console.ReadLine());
+            }
+            return result;
+        }
+
+        static void ShowMatNumbers(int[] mat, int i = 0)
+        {
+            if (mat.Length <= i )
+            {
+                return;
+            }
+
+            Console.WriteLine(mat[i]);
+            ShowMatNumbers(mat, i+1);
+        }
+    }
+}
